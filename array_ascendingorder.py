aa82e7fcc4ebb35c282f830f8ec09ec00eace931
aditya206237 <adityastudy673@gmail.com>
2025-02-26 23:38:32 +0530

    Date 26-02-25

---
+++ array_ascendingorder.py
@@ -0,0 +1,11 @@
+import array as arr
+a = arr.array('i', [12, 23, 99, 46, 57])
+temp=1
+for i in range(0,5):
+    for j in range (i):
+        if a[i]<a[j]:
+            temp=a[i]
+            a[i]=a[j]
+            a[j]=temp
+for i in range (0,5):
+    print(a[i])
