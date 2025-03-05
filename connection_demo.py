d6e35e6cfee8921066c060cd95967223d60826b5
aditya206237 <adityastudy673@gmail.com>
2025-03-05 23:56:49 +0530

    correct the coonection

--- connection_demo.py
+++ connection_demo.py
@@ -2,12 +2,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="host",
+    user="root",
     password="",
     database="aditya_python_db",
 )
 mycursor = mydb.cursor() # connect With Database
 
 sql = "INSERT INTO student_details (name, age) VALUES (%s, %s)"
+sql = "INSERT INTO details_of_student (name, email) VALUES (%s, %s)"
 val = ("Aditya","aditdtudy879@gmail.com")
 mycursor.execute(sql, val)
 
