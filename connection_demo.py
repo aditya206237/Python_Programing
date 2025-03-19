b7b4f5d9cf8049234cd4519b6af47ff1b69f4690
aditya206237 <adityastudy673@gmail.com>
2025-03-19 22:42:08 +0530

    modify

--- connection_demo.py
+++ connection_demo.py
@@ -3,13 +3,12 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="aditya_python_db",
+    database="adit_python_db",
 )
 mycursor = mydb.cursor() # connect With Database
 
 sql = "INSERT INTO details_of_student (name, email) VALUES (%s, %s)"
 val = ("Aditya","aditdtudy879@gmail.com")
 mycursor.execute(sql, val)
-
 mydb.commit() # Save Into Table
 print(mycursor.rowcount, "record inserted.")
