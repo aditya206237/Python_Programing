3e2fb3eec1ee3e1086c1fad1a501ee049383bf61
aditya206237 <adityastudy673@gmail.com>
2025-03-21 11:11:17 +0530

    Update whatsapp_msg.py

--- whatsapp_msg.py
+++ whatsapp_msg.py
@@ -1,4 +1,4 @@
 import pywhatkit as pwk
-phone_number="+918200958656"
+phone_number="+917990061449"
 message="Hello, This is a test message"
-pwk.sendwhatmsg(phone_number,message,22,57)
+pwk.sendwhatmsg(phone_number,message,23,28)
