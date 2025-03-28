6c436490f42ce30a598153e8b5ea33cf427be506
aditya206237 <adityastudy673@gmail.com>
2025-03-28 19:10:10 +0530

    28.03.25

--- claas_database.py
+++ claas_database.py
@@ -1,57 +1,57 @@
-class Database:
-    def setData(self,name,email,no):
+import mysql.connector
+class DataBase:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "",
+            database = "adit_python_db"
+        )
+        self.mycursor = self.mydb.cursor()
+    def insertData(self, name, email):
         self.name=name
         self.email=email
-        self.no=no
-    def import_data(self):
-        import mysql.connector
-        mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="adit_python_db",
-        )
-        mycursor=mydb.cursor()
-    def insert_data(self):
-        self.no = input("Enter the no: ")
-        self.name = input("Enter the name: ")
-        self.email = input("Enter the email: ")
-    def view_data(self):
+        sql = "INSERT INTO details_of_student (name, email) VALUES (%s, %s)"
+        val = (name, email)
+        self.mycursor.execute(sql, val)
+        self.mydb.commit()
+        print(self.mycursor.rowcount, "record inserted.")
+    def viewData(self):
         self.mycursor.execute("SELECT * FROM details_of_student")
-        result = self.mycursor.fetchall()
-        for row in result:
-            print(row)
-
-def __init__(self):
-    import mysql.connector
-    self.mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="adit_python_db",
-    )
-    self.mycursor = self.mydb.cursor()
-    def delete_data(self):
-        no = input("Enter the no of the record to delete: ")
-        sql = "DELETE FROM details_of_student WHERE no = %s"
-        val = (no,)
-        self.mycursor.execute(sql, val)
+        myresult = self.mycursor.fetchall()
+        for x in myresult:
+            print(x)
+    def deleteData(self):
+        no = input("Enter the ID of the record to delete:-")
+        sql = "DELETE FROM details_of_student WHERE id = %s"
+        self.mycursor.execute(sql, (no,))
         self.mydb.commit()
-        print("Data deleted successfully.")
-
-    def update_data(self):
-        no = input("Enter the no of the record to update: ")
-        name = input("Enter the new name: ")
-        email = input("Enter the new email: ")
-        sql = "UPDATE details_of_student SET name = %s, email = %s WHERE no = %s"
-        val = (name, email, no)
-        self.mycursor.execute(sql, val)
+        print(self.mycursor.rowcount, "record deleted.")
+    def updateData(self):
+        no = input("Enter the number to be updated:-")
+        name = input("Enter the new email:-")
+        email = input("Enter the new email:-")
+        sql = "UPDATE details_of_student SET name = %s, email = %s WHERE id = %s"
+        self.mycursor.execute(sql, (name, email, no))
         self.mydb.commit()
-        print("Data updated successfully.")
+        print(self.mycursor.rowcount, "record updated succesfully.")
 
-d = Database()
-d.import_data()
-d.insert_data()
-d.view_data()
-d.delete_data()
-d.update_data()
+print("For inserting data enter=>1")
+print("For viewing data enter=>2")
+print("For deleting data enter=>3")
+print("For updating data enter=>4")
+d=DataBase()
+choice = int(input("Enter your choice:-"))
+if choice==1:
+    name = input("Enter the name:-")
+    email = input("Enter the email:-")
+    d.insertData(name, email)
+elif choice==2:
+    d.viewData()
+elif choice==3:
+    d.deleteData()
+elif choice==4:
+    d.updateData()
+else:
+    print("Invalid choice")
+    
